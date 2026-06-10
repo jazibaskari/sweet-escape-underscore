9ad046e92d7272e4b48ea92fb6a9fb68a77cdf68
Jazib Askari <jazibaskari@hotmail.co.uk>
2026-06-10 20:41:25 +0100

    feat(melody): add main kalimba melody

--- src/sweet_escape.ts
+++ src/sweet_escape.ts
@@ -14,4 +14,10 @@ const getBassLayer = () =>
     .sustain(0.1)
     .gain(config.globalGain);
 
-stack(getBassLayer()).cpm(config.tempo);
+const getThemeLayer = () =>
+  note("<[c5 e5] ~ [c5 f5] ~ [g5 b5] ~ [a5 c6] ~>")
+    .cat("~ <[c5 e5] ~ [d5 f5] ~ [e5 g5] ~ [f5 a5] ~>")
+    .s("kalimba")
+    .gain(config.globalGain);
+
+stack(getBassLayer(), getThemeLayer()).cpm(config.tempo);
